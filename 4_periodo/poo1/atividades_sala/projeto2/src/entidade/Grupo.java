ba67082d5234379dc4561998bf5904651e5e81ef
douglasbolis <douglas.lima@xdevel.com.br>
2016-08-18 08:54:25 -0300

    Primeira versão do projeto 2 com pessoa e grupo.

---
+++ 4_periodo/poo1/atividades_sala/projeto2/src/entidade/Grupo.java
@@ -0,0 +1,15 @@
+package entidade;
+
+public class Grupo {
+    private Pessoa[] membros;
+    private int nMembros;
+
+    public Grupo() {
+        membros = new Pessoa[100];
+        nMembros = 0;
+    }
+
+    public int criaMembro(String nome, int idade) {
+        return 0;
+    }
+}
---
+++ 4_periodo/poo1/atividades_sala/projeto2/src/entidade/Pessoa.java
@@ -0,0 +1,19 @@
+package entidade;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIndade() {
+        return this.idade;
+    }
+}
