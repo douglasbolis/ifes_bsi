d14b780675cefc91719b1fd207d47c8abe15c289
douglasbolis <douglas.lima@xdevel.com.br>
2016-08-18 08:29:26 -0300

    Alterado Premier para instanciar um novo objeto de Sortie; Adicionado gitignore

--- 4_periodo/poo1/atividades_sala/projeto0/src/controle/Premier.java
+++ 4_periodo/poo1/atividades_sala/projeto0/src/controle/Premier.java
@@ -1,5 +1,8 @@
+import fronteira.*;
+
 public class Premier {
     public static void main(String[] args) {
-        fronteira.Sortie.print("Ça marche bien!\n");
+        Sortie sortie = new Sortie();
+        sortie.print("Ça marche bien!\n");
     }
 }
