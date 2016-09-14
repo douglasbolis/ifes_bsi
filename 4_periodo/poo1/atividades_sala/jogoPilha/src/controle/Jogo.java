3b64fbcc59aac6b653b19635f06032e2bf54fc63
douglasbolis <douglas.lima@xdevel.com.br>
2016-09-14 14:49:07 -0300

    Finalizado jogoPilha

--- 4_periodo/poo1/atividades_sala/jogoPilha/src/controle/Jogo.java
+++ 4_periodo/poo1/atividades_sala/jogoPilha/src/controle/Jogo.java
@@ -29,15 +29,39 @@ public class Jogo {
         
         // Jogador 2 tentando adivinhar um dos números informados pelo Jogador 1
         Saida.print("\nVez do jogador 2\n");
-        j2.setNumEscolhido(Entrada.leInt("Digite um número que esteja na pilha: "));
+        j2.setNumEscolhido(Entrada.leInt("Digite um número e tente adivinhar um dos números que esteja na pilha: "));
         
         calculaPontuacao(p, j2, ctrl, achou);
 
         // Zerando controles
         ctrl = 0;
         achou = false;
+        p.limpaPilha();
+
+        // Jogador 2 digitando os ${tamPilha} numeros
+        Saida.print("\nJogador 2\n");
+        Saida.print("Digite " + p.getTamMax() + " números\n");
+
+        for (int x = 0; x < p.getTamMax(); x++) {
+            p.empilhe(Entrada.leInt((x + 1) + "º: "));
+        }
         
+        // Jogador 2 tentando adivinhar um dos números informados pelo Jogador 1
+        Saida.print("\nJogador 1\n");
+        j1.setNumEscolhido(Entrada.leInt("Digite um número e tente advinhar um dos números que esteja na pilha: "));
+        
+        calculaPontuacao(p, j1, ctrl, achou);
+        
+        Saida.print("\nPontuação Jogador 1: " + j1.getPontuacao());
         Saida.print("\nPontuação Jogador 2: " + j2.getPontuacao() + "\n");
+
+        if (j1.getPontuacao() == j2.getPontuacao()) {
+            Saida.print("\nEmpate\n");
+        } else if (j1.getPontuacao() > j2.getPontuacao()) {
+            Saida.print("\nJogador 1 ganhou\n");
+        } else {
+            Saida.print("\nJogador 2 ganhou\n");
+        }
     }
 
     private void calculaPontuacao(Pilha p, Jogador j, int ctrl, Boolean achou) {
