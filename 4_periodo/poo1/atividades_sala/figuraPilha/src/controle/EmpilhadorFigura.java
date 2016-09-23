192b11cf2c49bb31b872aa235502543ff4fc2d5b
douglasbolis <douglas.lima@xdevel.com.br>
2016-09-23 13:05:02 -0300

    Adicionado verificação de inserção de triangulo(não funcionou corretamente)

--- 4_periodo/poo1/atividades_sala/figuraPilha/src/controle/EmpilhadorFigura.java
+++ 4_periodo/poo1/atividades_sala/figuraPilha/src/controle/EmpilhadorFigura.java
@@ -3,9 +3,11 @@ package controle;
 import entidades.*;
 import fronteira.*;
 
+import java.math.*;
+
 public class EmpilhadorFigura {
     private Pilha p;
-    private Figura f, fAnt;
+    private Figura fAnt;
     private int x, y, ladoRaio;
     private String opcao;
     private String []figuras = {"circulo", "quadrado", "triangulo"};
@@ -26,69 +28,70 @@ public class EmpilhadorFigura {
             switch (opcao) {
                 case "circulo":
                     ladoRaio = Entrada.leInt("\nInforme o raio do circulo: ");
-                    f = new Circulo(x, y, ladoRaio);
+                    Circulo nCirc = new Circulo(x, y, ladoRaio);
 
-                    // if (!p.vazia()) {
-                    //     fAnt = (Figura) p.topo();
-                    //     if (fAnt instanceof Circulo) {
-                    //         while(fAnt.getRaio() <= f.getRaio()) {
-                    //             Saida.print("O raio do novo circulo precisa ser menor que " + fAnt.getRaio());
-                    //             ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
-                    //             f.setRaio(ladoRaio);
-                    //         }
-                    //     } else if (fAnt instanceof Quadrado) {
-                    //         while(fAnt.getLado() <= (f.getRaio() * 2)) {
-                    //             Saida.print("O raio do novo circulo precisa ser menor que " + (fAnt.getLado() / 2));
-                    //             ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
-                    //             f.setRaio(ladoRaio);
-                    //         }
-                    //     } else if (fAnt instanceof Triangulo) {
-                    //         while((fAnt.getAltura() / 3) <= f.getRaio())) {
-                    //             Saida.print("O raio do novo circulo precisa ser menor que " + (fAnt.getAltura() / 3));
-                    //             ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
-                    //             f.setRaio(ladoRaio);
-                    //         }
-                    //     }
-                    // }
-                    p.empilhe(f);
+                    if (!p.vazia()) {
+                        fAnt = (Figura) p.topo();
+                        if (fAnt instanceof Circulo) {
+                            Circulo c = (Circulo) fAnt;
+                            while(c.getRaio() <= nCirc.getRaio()) {
+                                Saida.print("O raio do novo circulo precisa ser menor que " + c.getRaio());
+                                ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
+                                nCirc.setRaio(ladoRaio);
+                            }
+                        } else if (fAnt instanceof Quadrado) {
+                            Quadrado q = (Quadrado) fAnt;
+                            while(q.getLado() <= (nCirc.getRaio() * 2)) {
+                                Saida.print("O raio do novo circulo precisa ser menor que " + (q.getLado() / 2));
+                                ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
+                                nCirc.setRaio(ladoRaio);
+                            }
+                        } else if (fAnt instanceof Triangulo) {
+                            Triangulo t = (Triangulo) fAnt;
+                            while((t.getAltura() / 3) <= nCirc.getRaio()) {
+                                Saida.print("O raio do novo circulo precisa ser menor que " + Math.round(t.getAltura() / 3));
+                                ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
+                                nCirc.setRaio(ladoRaio);
+                            }
+                        }
+                    }
+                    p.empilhe(nCirc);
                     break;
                 case "quadrado":
                     ladoRaio = Entrada.leInt("\nInforme o lado do quadrado: ");
-                    f = new Quadrado(x, y, ladoRaio);
-
-                    // if (!p.vazia()) {
-                    //     fAnt = (Figura) p.topo();
-                    //     if (fAnt instanceof Circulo) {
-                    //         while(fAnt.getRaio() <= f.getRaio()) {
-                    //             Saida.print("O raio do novo circulo precisa ser menor que " + fAnt.getRaio());
-                    //             ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
-                    //             f.setRaio(ladoRaio);
-                    //         }
-                    //     } else if (fAnt instanceof Quadrado) { // OK
-                    //         while(fAnt.getLado() <= f.getLado()) {
-                    //             Saida.print("O lado do novo quadrado precisa ser menor que " + f.getLado());
-                    //             ladoRaio = Entrada.leInt("\nInforme novamente o lado do quadrado: ");
-                    //             f.setLado(ladoRaio);
-                    //         }
-                    //     } else if (fAnt instanceof Triangulo) {
-                    //         while((fAnt.getAltura() / 3) <= f.getRaio())) {
-                    //             Saida.print("O raio do novo circulo precisa ser menor que " + (fAnt.getAltura() / 3));
-                    //             ladoRaio = Entrada.leInt("\nInforme novamente o raio do circulo: ");
-                    //             f.setRaio(ladoRaio);
-                    //         }
-                    //     }
-                    // }
-                    p.empilhe(f);
+                    Quadrado nQuad = new Quadrado(x, y, ladoRaio);
+                    p.empilhe(nQuad);
                     break;
                 case "triangulo":
                     ladoRaio = Entrada.leInt("\nInforme o lado do triangulo: ");
-                    f = new Triangulo(x, y, ladoRaio);
-                    p.empilhe(f);
+                    Triangulo nTrian = new Triangulo(x, y, ladoRaio);
 
-                    // if (p.vazia()) {
-                    // } else {
-                    //     fAnt = (Figura) p.topo();
-                    // }
+                    if (!p.vazia()) {
+                        fAnt = (Figura) p.topo();
+                        if (fAnt instanceof Circulo) {
+                            Circulo c = (Circulo) fAnt;
+                            while(c.getRaio() <= (nTrian.getAltura() * (2 / 3))) {
+                                Saida.print("O lado do novo triangulo precisa ser menor que " + Math.round((3 * (c.getRaio() / 2)) / (Math.sqrt(2) / 2)));
+                                ladoRaio = Entrada.leInt("\nInforme novamente o lado do triangulo: ");
+                                nTrian.setLado(ladoRaio);
+                            }
+                        } else if (fAnt instanceof Quadrado) {
+                            Quadrado q = (Quadrado) fAnt;
+                            while((q.getLado() / 2) <= (nTrian.getAltura() * (2 / 3))) {
+                                Saida.print("O lado do novo triangulo precisa ser menor que " + Math.round((3 * (q.getLado() / 4)) / (Math.sqrt(2) / 2)));
+                                ladoRaio = Entrada.leInt("\nInforme novamente o lado do triangulo: ");
+                                nTrian.setLado(ladoRaio);
+                            }
+                        } else if (fAnt instanceof Triangulo) {
+                            Triangulo t = (Triangulo) fAnt;
+                            while((t.getAltura() / 3) <= ((nTrian.getAltura() * 2) / 3)) {
+                                Saida.print("O lado do novo triangulo precisa ser menor que " + Math.round((3 * (t.getAltura() / 6)) / (Math.sqrt(2) / 2)));
+                                ladoRaio = Entrada.leInt("\nInforme novamente o lado do triangulo: ");
+                                nTrian.setLado(ladoRaio);
+                            }
+                        }
+                    }
+                    p.empilhe(nTrian);
                     break;
                 default:
                     break;
